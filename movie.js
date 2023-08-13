f6bce91fabe13e7a3c362b0496b897465f040e45
Daryna Holikova <dgolikova0@gmail.com>
2023-08-13 21:16:11 +0100

    movie

---
+++ movie.js
@@ -0,0 +1,19 @@
+ // const API_KEY = "92a9a9e3708a3e9451b7037d5906879a";
+
+
+//  const BASE_URL = "https://api.themoviedb.org/3";
+//  const END_POINT = "/trending/movie/week";
+
+const refs = {
+    movieList: document.getElementById("movieList"),
+    loadMore: document.getElementById("loadMore"),
+  };
+
+
+
+function serviceMovie(){
+    const BASE_URL = "https://api.themoviedb.org/3";
+    const END_POINT = "/trending/movie/week";
+    const API_KEY = "92a9a9e3708a3e9451b7037d5906879a";
+    
+}
